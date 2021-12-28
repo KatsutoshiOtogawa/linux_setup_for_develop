b648e9488fd08f266c0a23774fe95e472546ad7c
KatsutoshiOtogawa <13373011+KatsutoshiOtogawa@users.noreply.github.com>
2021-12-28 12:42:55 +0900

    Create provision.sh

---
+++ provision.sh
@@ -0,0 +1,8 @@
+
+dnf install -y git
+
+# install Anaconda don't need root priveledge.
+curl -O https://repo.anaconda.com/archive/Anaconda3-5.3.1-Linux-x86_64.sh
+chmod u+x Anaconda3*
+./Anaconda3*
+
