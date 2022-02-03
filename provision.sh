d469f681fabdc93236467e9ec36850d8b62a29da
KatsutoshiOtogawa <13373011+KatsutoshiOtogawa@users.noreply.github.com>
2022-02-03 23:13:45 +0900

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,6 +1,11 @@
 
 dnf install -y git
 
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
 # install Anaconda don't need root priveledge.
 curl -O https://repo.anaconda.com/archive/Anaconda3-5.3.1-Linux-x86_64.sh
 chmod u+x Anaconda3*
